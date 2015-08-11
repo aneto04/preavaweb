cae5b89945c81d3c51a51cc04b672d8d1822102c
Armando Araújo <netoarmando@outlook.com>
2015-08-11 11:58:41 -0300

    versão 1.3.2

--- cadastroAvaWeb/cadastrando.php
+++ cadastroAvaWeb/cadastrando.php
@@ -7,7 +7,6 @@
         $nome_completo = $_POST['nomeCompleto'];
         $nome_exibido = $_POST['nomeExibido'];
         $entrada = $_POST['dataEntrada'];
-        $cargo = $_POST['cargo'];
         $cpf = $_POST['cpf'];
         $rg = $_POST['rg'];
         $nascimento = $_POST['dataNascimento'];
@@ -21,16 +20,15 @@
         $email = $_POST['email'];
         $senha = $_POST['senha'];
 
-        $sql = 'INSERT INTO usuarios(username, nome_completo, nome_exibido, entrada, cargo, cpf, rg, nascimento, nome_mae, nome_pai, endereco, num_casa, bairro, cep, telefone, email, senha)
-        VALUES (:username, :nome_completo, :nome_exibido, :entrada, :cargo, :cpf, :rg, :nascimento, :nome_mae, :nome_pai, :endereco, :num_casa, :bairro, :cep, :telefone, :email, :senha)';
+        $sql = 'INSERT INTO usuarios(username, nome_completo, nome_exibido, entrada, cpf, rg, nascimento, nome_mae, nome_pai, endereco, num_casa, bairro, cep, telefone, email, senha)
+            VALUES (:username, :nome_completo, :nome_exibido, :entrada, :cpf, :rg, :nascimento, :nome_mae, :nome_pai, :endereco, :num_casa, :bairro, :cep, :telefone, :email, :senha)';
 
         try {
             $create = $db -> prepare($sql);
             $create -> bindValue(':username', $username, PDO::PARAM_STR);
             $create -> bindValue(':nome_completo', $nome_completo, PDO::PARAM_STR);
-            $create -> bindValue(':nome_exibido', $nome_exibido, PDO::PARAM_STR);
+            $create -> bindValue(':nome_exibido', $nome_completo, PDO::PARAM_STR);
             $create -> bindValue(':entrada', $entrada, PDO::PARAM_STR);
-            $create -> bindValue(':cargo', $cargo, PDO::PARAM_STR);
             $create -> bindValue(':cpf', $cpf, PDO::PARAM_STR);
             $create -> bindValue(':rg', $rg, PDO::PARAM_STR);
             $create -> bindValue(':nascimento', $nascimento, PDO::PARAM_STR);
@@ -47,7 +45,6 @@
             if ($create->execute()) {
                 echo "Sucesso ao inserir dados;";
             }
-
         } catch (PDOException $e) {
             echo "Erro ao inserir dados;";
         }
